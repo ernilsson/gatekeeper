a12d3c11bed3665a0a93bf9382229734fc806b62
E. R. Nilsson <e.rasmus.nilsson@gmail.com>
2024-08-03 14:39:04 +0200

    Adds initial test for Split function, currently missing cases where node is not a leaf

---
+++ internal/gaslight/internal/dal/node_test.go
@@ -0,0 +1,95 @@
+package dal
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestSplit(t *testing.T) {
+	provideItem := func(id string) *Item {
+		return &Item{
+			key:   []byte("key_" + id),
+			value: []byte("value_" + id),
+		}
+	}
+	matrix := []struct {
+		name     string
+		node     *Node
+		expected [2]*Node
+	}{
+		{
+			name: "given even amount of items on leaf node",
+			node: &Node{
+				items: []*Item{
+					provideItem("one"),
+					provideItem("two"),
+					provideItem("three"),
+					provideItem("four"),
+				},
+			},
+			expected: [2]*Node{
+				{
+					items: []*Item{
+						provideItem("one"),
+						provideItem("two"),
+					},
+				},
+				{
+					items: []*Item{
+						provideItem("four"),
+					},
+				},
+			},
+		},
+		{
+			name: "given odd amount of items on leaf node",
+			node: &Node{
+				items: []*Item{
+					provideItem("one"),
+					provideItem("two"),
+					provideItem("three"),
+				},
+			},
+			expected: [2]*Node{
+				{
+					items: []*Item{
+						provideItem("one"),
+					},
+				},
+				{
+					items: []*Item{
+						provideItem("three"),
+					},
+				},
+			},
+		},
+	}
+
+	unequal := func(a, b *Node) bool {
+		for i := range a.items {
+			if bytes.Compare(a.items[i].key, b.items[i].key) != 0 {
+				return true
+			}
+			if bytes.Compare(a.items[i].value, b.items[i].value) != 0 {
+				return true
+			}
+		}
+		for i := range a.children {
+			if a.children[i] != b.children[i] {
+				return true
+			}
+		}
+		return false
+	}
+	for _, m := range matrix {
+		t.Run(m.name, func(t *testing.T) {
+			a, b := Split(m.node)
+			if unequal(m.expected[0], a) {
+				t.Fatalf("got %v; want %v", a, m.expected[0])
+			}
+			if unequal(m.expected[1], b) {
+				t.Fatalf("got %v; want %v", b, m.expected[1])
+			}
+		})
+	}
+}
